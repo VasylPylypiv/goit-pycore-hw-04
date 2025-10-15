97c7aa03b6009e131067e7e46cb1abda85b16f75
Vasyl Pylypiv <vasyl.pylypiv@ringteam.com>
2025-10-15 16:20:04 +0300

    Small refactoring

--- task1.py
+++ task1.py
@@ -3,10 +3,7 @@ from pathlib import Path
 def total_salary(path):
     try:
         file_path = Path(path)
-        if not file_path.exists():
-            print(f"{file_path} не існує")
-            return 0,0
-        
+                
         with file_path.open("r", encoding="utf-8") as file:
             total = 0
             count = 0
@@ -18,7 +15,7 @@ def total_salary(path):
                     total += salary
                     count += 1
                 except ValueError:
-                    print("Невірний формат даних у рядку: {line}")
+                    print(f"Невірний формат даних у рядку: {line}")                    
                     continue
             if count > 0:
                 average = total/count
